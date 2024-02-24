8d9849b533a4bc49185bb68823839a0cddb61ed4
hyerinnn <eun8229@gmail.com>
2024-02-25 02:37:31 +0900

    DB동작확인 및 CSS,JS 파일 추가

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +12,13 @@ import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Rollback(false)
 public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
     @Test
-    @Transactional
+    @Transactional  // 테스트에 있으면 테스트끝나자마자 롤백을 시킴 -> 그래서 데이터 조회가 안됨 -> @Rollback(false)붙이면 됨
     public void testMember() throws Exception{
 
         Member member  = new Member();
